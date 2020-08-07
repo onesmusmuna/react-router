583bb46e46b61447d5293d1799f7c6b847734897
onesmusmuna-n <onesmusjones@gmail.com>
2020-08-07 18:17:11 +0300

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-// import ProductDetails from "./components/productDetails";
+import ProductDetails from "./components/productDetails";
 // import NotFound from "./components/notFound";
 import "./App.css";
 
@@ -17,6 +17,7 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
+            <Route path={"/products/:id"} component={ProductDetails} />
             <Route path={"/products"} render={(props) => <Products {...props} sortBy={"newest"} />} />
             <Route path={"/posts"} component={Posts} />
             <Route path={"/admin"} component={Dashboard} />
