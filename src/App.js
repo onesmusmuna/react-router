57d1e2bb9bb5f54c55d950010a848c88446de48e
onesmusmuna-n <onesmusjones@gmail.com>
2020-08-07 17:54:55 +0300

    Passing Props

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
+// import ProductDetails from "./components/productDetails";
+// import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
@@ -17,7 +17,7 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path={"/products"} component={Products} />
+            <Route path={"/products"} render={(props) => <Products {...props} sortBy={"newest"} />} />
             <Route path={"/posts"} component={Posts} />
             <Route path={"/admin"} component={Dashboard} />
             <Route path={"/"} component={Home} />
