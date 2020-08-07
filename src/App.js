b8b4e9d574f5e1148dde1e7dbdc8c66b725e8d76
onesmusmuna-n <onesmusjones@gmail.com>
2020-08-07 18:37:15 +0300

    Optional Parameters: In Posts route, i have made the :year and :month as optional parameters, by adding a Question mark after them.
    This is part of regular expression, when you append a ? in a reg expression, that means that reg expression is optional

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           <Switch>
             <Route path={"/products/:id"} component={ProductDetails} />
             <Route path={"/products"} render={(props) => <Products {...props} sortBy={"newest"} />} />
-            <Route path={"/posts"} component={Posts} />
+            <Route path={"/posts/:year?/:month?"} component={Posts} />
             <Route path={"/admin"} component={Dashboard} />
             <Route path={"/"} component={Home} />
           </Switch>
